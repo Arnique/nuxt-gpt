3a4017119755a8eb35f4c6267ab3c43aceec8fa1
arnique <arnold@arnique.net>
2024-08-10 18:41:15 +0300

    fix env

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,30 +1,30 @@
 const {
-  apiKey,
-  appId,
-  authDomain,
-  messagingSenderId,
-  projectId,
-  storageBucket,
-  adminEmails,
-  authCookie,
-  openaiKey,
+  NUXT_PUBLIC_FB_API_KEY,
+  NUXT_PUBLIC_FB_APP_ID,
+  NUXT_PUBLIC_FB_AUTH_DOMAIN,
+  NUXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+  NUXT_PUBLIC_FB_PROJECT_ID,
+  NUXT_PUBLIC_FB_STORAGE_BUCKET,
+  NUXT_ADMIN_EMAILS,
+  NUXT_AUTH_COOKIE,
+  NUXT_OPENAI_KEY,
 } = process.env
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   runtimeConfig: {
-    authCookie,
-    adminEmails,
-    openaiKey,
+    authCookie: NUXT_AUTH_COOKIE,
+    adminEmails: NUXT_ADMIN_EMAILS,
+    openaiKey: NUXT_OPENAI_KEY,
     public: {
       maxTokens: 100,
       fb: {
-        apiKey,
-        appId,
-        authDomain,
-        messagingSenderId,
-        projectId,
-        storageBucket,
+        apiKey: NUXT_PUBLIC_FB_API_KEY,
+        appId: NUXT_PUBLIC_FB_APP_ID,
+        authDomain: NUXT_PUBLIC_FB_AUTH_DOMAIN,
+        messagingSenderId: NUXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+        projectId: NUXT_PUBLIC_FB_PROJECT_ID,
+        storageBucket: NUXT_PUBLIC_FB_STORAGE_BUCKET,
       }
     }
   },
